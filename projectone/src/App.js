6878648f4f62a19a2626e228946df2bddbc28288
aman chouksey <amanchouksey0@gmail.com>
2023-05-10 18:58:06 +0530

    projectone

---
+++ projectone/src/App.js
@@ -0,0 +1,18 @@
+
+import {BrowserRouter as Router} from "react-router-dom" 
+import Header from "./components/Header";
+
+import "./styles/App.scss";
+import "./styles/header.scss";
+
+function App() {
+    return (
+        <Router>
+          <Routes>
+            <Route />
+          </Routes>
+        </Router>
+    );
+}
+
+export default App;
---
+++ projectone/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return <nav>
+    <h1>TechStar.</h1>
+    <main>
+
+        <link to={"/"}>Home</link>
+        <link to={"/contact"}>Contact</link>
+        <link to={"/"}>About</link>
+        <link to={"/"}>Home</link>
+        <link to={"/"}>Home</link>
+    </main>
+  </nav> ;
+};
+
+export default Header;
